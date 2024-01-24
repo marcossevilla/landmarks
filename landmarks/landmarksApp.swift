c828574f0495db7c4699cdbd0b007b963897d3f5
Marcos Sevilla <marcossevilla14@gmail.com>
2024-01-24 12:27:40 +0100

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Marcos Sevilla on 24/1/24.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
