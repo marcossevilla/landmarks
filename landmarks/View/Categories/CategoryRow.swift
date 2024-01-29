43fa9c3a71c4a3e49a7c66b111d618e7f94ff894
Marcos Sevilla <marcossevilla14@gmail.com>
2024-01-29 14:42:06 +0100

    feat: create a category row

---
+++ landmarks/View/Categories/CategoryRow.swift
@@ -0,0 +1,39 @@
+//
+//  CategoryRow.swift
+//  landmarks
+//
+//  Created by Marcos Sevilla on 29/1/24.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.top, 5)
+                .padding(.leading, 15)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        CategoryItem(landmark: landmark)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+#Preview {
+    let landmarks = ModelData().landmarks
+    return CategoryRow(
+        categoryName: landmarks.first!.name,
+        items: Array(landmarks.prefix(4))
+    )
+}
