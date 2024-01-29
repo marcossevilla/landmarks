efed156188d4b46301b2e573f629dcab6cb50dcb
Marcos Sevilla <marcossevilla14@gmail.com>
2024-01-29 14:23:55 +0100

    feat: create a category list

--- landmarks/View/Categories/CategoryHome.swift
+++ landmarks/View/Categories/CategoryHome.swift
@@ -8,10 +8,16 @@
 import SwiftUI
 
 struct CategoryHome: View {
+    @Environment(ModelData.self) var modelData
+    
     var body: some View {
         NavigationSplitView {
-            Text("Hello, world!")
-                .navigationTitle("Featured")
+            List {
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
+            .navigationTitle("Featured")
         } detail: {
             Text("Select a landmark")
         }
@@ -19,5 +25,5 @@ struct CategoryHome: View {
 }
 
 #Preview {
-    CategoryHome()
+    CategoryHome().environment(ModelData())
 }
