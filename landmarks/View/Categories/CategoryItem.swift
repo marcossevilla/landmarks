60dd01e414272d00725795aaf23fb4e2f0128e19
Marcos Sevilla <31174242+marcossevilla@users.noreply.github.com>
2024-01-29 15:15:40 +0100

    feat: app design layout (#5)
    * feat: add category view
    
    * feat: create a category list
    
    * feat: create a category row
    
    * feat: complete category view
    
    * feat: add navigation between sections

---
+++ landmarks/View/Categories/CategoryItem.swift
@@ -0,0 +1,31 @@
+//
+//  CategoryItem.swift
+//  landmarks
+//
+//  Created by Marcos Sevilla on 29/1/24.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .renderingMode(.original)
+                .resizable()
+                .cornerRadius(5)
+                .frame(width: 155, height: 155)
+            
+            Text(landmark.name)
+                .foregroundStyle(.primary)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+#Preview {
+    CategoryItem(landmark: ModelData().landmarks.first!)
+}
